7f95b3e45371b4c770f9fb997d9ff2dc57e2125f
HamletPoghosian <phamik@mail.ru>
2019-05-23 17:08:12 +0400

    Add new property for Class

--- Online_Book_Store/Online_Book_Store.BookStore/Data/ShopingCart.cs
+++ Online_Book_Store/Online_Book_Store.BookStore/Data/ShopingCart.cs
@@ -7,5 +7,9 @@ namespace Online_Book_Store.BookStore.Data
 {
     public class ShopingCart
     {
+        public Guid Id { get; set; }
+        public Guid ApplicationUserId { get; set; }
+        public Book Book { get; set; }
+        public int Amount { get; set; }
     }
 }
