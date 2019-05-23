7cd64c86f082f254a4821defb609defdfcb71d00
HamletPoghosian <phamik@mail.ru>
2019-05-23 17:07:34 +0400

    Add new prop for Class

--- Online_Book_Store/Online_Book_Store.BookStore/Data/Book.cs
+++ Online_Book_Store/Online_Book_Store.BookStore/Data/Book.cs
@@ -7,5 +7,14 @@ namespace Online_Book_Store.BookStore.Data
 {
     public class Book
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public double Popular { get; set; }
+        public double Price { get; set; }
+        public DateTime Publish { get; set; }
+
+        public Guid CategoryId { get; set; }
+        public Category Category { get; set; }
     }
 }
