3bdc92b1d545b0c3b95b95a702702eb00f89ee08
HamletPoghosian <phamik@mail.ru>
2019-05-23 17:10:27 +0400

    Add new class

---
+++ Online_Book_Store/Online_Book_Store.BookStore/Service/BookService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Online_Book_Store.BookStore.Service
+{
+    public class BookService
+    {
+    }
+}
