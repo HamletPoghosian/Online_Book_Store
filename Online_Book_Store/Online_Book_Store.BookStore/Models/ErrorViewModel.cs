20aef4a8a50723ec1d8a5c649ff4a94a02bfa4e8
HamletPoghosian <phamik@mail.ru>
2019-05-23 17:03:06 +0400

    Add new project  Asp .New MVC Core Book_Store

---
+++ Online_Book_Store/Online_Book_Store.BookStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Online_Book_Store.BookStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
