6c0c8e38a5f72bd6c51f65224a1f19abd9349438
HamletPoghosian <phamik@mail.ru>
2019-05-24 12:11:22 +0400

    Add new Category Service Logics

---
+++ Online_Book_Store/Online_Book_Store.BookStore/Service/CategoryService/CategoryServic.cs
@@ -0,0 +1,53 @@
+﻿using Online_Book_Store.BookStore.Data;
+using Online_Book_Store.BookStore.Service.CategoryService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Online_Book_Store.BookStore.Service.Category
+{
+    public  class CategoryServic:ICategoryService
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryServic(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Data.Category> AddItemsAsync(Data.Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Data.Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task EditAsync(Data.Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Book> GetCategoryAsync(Guid categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<string>> GetCategoryDescription()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<string>> GetCategoryName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Data.Category>> GetCategorysAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Online_Book_Store/Online_Book_Store.BookStore/Service/CategoryService/ICategoryService.cs
@@ -0,0 +1,19 @@
+﻿using Online_Book_Store.BookStore.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Online_Book_Store.BookStore.Service.CategoryService
+{
+    interface ICategoryService
+    {
+        Task<Data.Category> AddItemsAsync(Data.Category category);
+        Task EditAsync(Data.Category category);
+        Task DeleteAsync(Data.Category category);
+        Task<Book> GetCategoryAsync(Guid categoryId);
+        Task<IEnumerable<Data.Category>> GetCategorysAsync();
+        Task<List<string>> GetCategoryName();
+        Task<List<string>> GetCategoryDescription();
+    }
+}
