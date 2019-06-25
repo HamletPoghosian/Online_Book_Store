5daf10ca9b901e979032c8e9a033c2716a06af51
HamletPoghosian <phamik@mail.ru>
2019-06-25 16:32:39 +0400

    bug fix

--- Online_Book_Store/Online_Book_Store.BookStore/Models/ShopingCartModel.cs
+++ Online_Book_Store/Online_Book_Store.BookStore/Models/ShopingCartModel.cs
@@ -8,8 +8,11 @@ namespace Online_Book_Store.BookStore.Models
     public class ShopingCartModel
     {
         public Guid Id { get; set; }
+
         public ViewBook Book { get; set; }
+
         public int Amount { get; set; }
+
         public double TotalPrice { get; set; }
     }
 }
