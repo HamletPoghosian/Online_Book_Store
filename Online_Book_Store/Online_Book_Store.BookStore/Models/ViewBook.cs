fc73efd6028ab317a74e72dae16c4b42c5a63cf1
HamletPoghosian <phamik@mail.ru>
2019-06-25 16:33:02 +0400

    bug fix

--- Online_Book_Store/Online_Book_Store.BookStore/Models/ViewBook.cs
+++ Online_Book_Store/Online_Book_Store.BookStore/Models/ViewBook.cs
@@ -8,13 +8,21 @@ namespace Online_Book_Store.BookStore.Models
     public class ViewBook
     {
         public Guid Id { get; set; }
+
         public string Name { get; set; }
+
         public string Author { get; set; }
+
         public double Popular { get; set; }
+
         public DateTime Publish { get; set; }
+
         public double Price { get; set; }
+
         public int Amount { get; set; }
-        public Guid CategoryId { get; set; }      
+
+        public Guid CategoryId { get; set; }   
+        
         public string CategoryName { get; set; }
         
     }
