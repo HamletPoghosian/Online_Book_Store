b8e886770073b1663a649a0d92676e72a4a35692
HamletPoghosian <phamik@mail.ru>
2019-07-01 13:02:42 +0400

    bug fixed

--- Online_Book_Store/Online_Book_Store.BookStore/Service/CategoryService/ICategoryService.cs
+++ Online_Book_Store/Online_Book_Store.BookStore/Service/CategoryService/ICategoryService.cs
@@ -9,12 +9,20 @@ namespace Online_Book_Store.BookStore.Service.CategoryService
     public interface ICategoryService
     {
         Task<Data.Category> AddItemsAsync(Data.Category category);
+
         Task EditAsync(Data.Category category);
+
         Task DeleteAsync(Data.Category category);
-        Task<Data.Category> GetCategoryAsync(Guid categoryId);        
+
+        Task<Data.Category> GetCategoryAsync(Guid categoryId); 
+        
         List<string> GetCategoryName();
+
         List<string> GetCategoryDescription();
+
         Task<IEnumerable<Data.Category>> GetCategorysAsync();
+
         IEnumerable<Data.Category> GetCategory();
+
     }
 }
